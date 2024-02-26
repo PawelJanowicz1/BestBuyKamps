0456ef51340d49fa2925d26c16b22999ef772692
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-02-26 20:25:50 +0100

    fixes

--- src/test/java/com/bestbuykamps/websiteshop/WebsiteShopApplicationTests.java
+++ src/test/java/com/bestbuykamps/websiteshop/WebsiteShopApplicationTests.java
@@ -1,13 +1,9 @@
 package com.bestbuykamps.websiteshop;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class WebsiteShopApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
